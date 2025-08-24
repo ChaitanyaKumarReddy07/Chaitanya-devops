bcbc565ec413dbfde9877e1e6c28546c2366e89b
Chaitanya Kumar Reddy <157815652+ChaitanyaKumarReddy07@users.noreply.github.com>
2025-08-25 04:18:43 +0530

    Update backend.tf

--- Jenkins-Server-TF/backend.tf
+++ Jenkins-Server-TF/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
-    region         = "us-east-1"
-    key            = "End-to-End-Kubernetes-Three-Tier-DevSecOps-Project/Jenkins-Server-TF/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
+    bucket         = "dev-chaitanya-tf-bucket"   # Correct bucket name
+    key            = "eks/terraform.tfstate"
+    region         = "ap-southeast-2"            # Correct region
+    dynamodb_table = "chaitanya-tf-lock"         # Make sure this exists
     encrypt        = true
   }
   required_version = ">=0.13.0"
